f242ad8ce7e8f58d8451ac11bb9c9b748dfb16f8
Mani <140153681+MXNIHACKER@users.noreply.github.com>
2023-09-25 17:40:17 +0530

    config.py

--- config.py
+++ config.py
@@ -78,21 +78,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/22f698f4a50979fa296f9.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/1a806bb12b6ed8cfd975c.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/e66ea83e2ea8ea70071ed.jpg"
 
 
 def time_to_seconds(time):
@@ -101,13 +101,6 @@ def time_to_seconds(time):
 
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
-
-
-if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
-        raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
-        )
 
 if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
