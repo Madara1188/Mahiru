d21a706e505e959410c1d82f3fa5d5c22db8b6c8
Mani <140153681+MXNIHACKER@users.noreply.github.com>
2023-10-12 11:24:59 +0530

    config.py

--- config.py
+++ config.py
@@ -35,9 +35,6 @@ UPSTREAM_REPO = getenv(
     "https://github.com/MXNIHACKER/DevilXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Devils_Hell_0")
 
