829914da608ace5c5fc0777e20d4f1ad76834181
Mani <140153681+MXNIHACKER@users.noreply.github.com>
2023-10-12 11:39:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,9 +32,13 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/MXNIHACKER/DevilXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+)  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Devils_Hell_0")
 
