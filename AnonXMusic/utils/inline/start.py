e4a3483315cdd7440fd8e57f4964b3232f879190
GAMING CHAUHAN <143026011+Gamingbuddyyy@users.noreply.github.com>
2023-11-02 21:18:10 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -12,7 +12,7 @@ def start_panel(_):
             ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [   InlineKeyboardButton(text="🍃𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑🍃", url="https://t.me/II_mxni_II"),
+        [   InlineKeyboardButton(text="🍃𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑🍃", url="https://t.me/gamingggggg3"),
         ]
     ]
     return buttons
@@ -31,7 +31,7 @@ def private_panel(_):
             InlineKeyboardButton(text="sᴏᴜʀᴄᴇ", url="https://t.me/im_ur_dad_baby"),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [   InlineKeyboardButton(text="🇨🇦ᴅᴇᴠᴇʟᴏᴘᴇʀ🇨🇦", url="https://t.me/II_mxni_II"),
+        [   InlineKeyboardButton(text="🇨🇦ᴅᴇᴠᴇʟᴏᴘᴇʀ🇨🇦", url="https://t.me/gamingggggg3"),
         ],
     ]
     return buttons
