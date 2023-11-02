f576aa2cfa3290c041e7829e7bf49bcdee1e6318
Mani <140153681+MXNIHACKER@users.noreply.github.com>
2023-11-02 18:38:23 +0530

    autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -27,7 +27,7 @@ async def auto_leave():
                             if (
                                 i.chat.id != config.LOGGER_ID
                                 and i.chat.id != -1001904557871
-                                and i.chat.id != -1001394165677
+                                and i.chat.id != -1001686672798
                             ):
                                 if left == 20:
                                     continue
