371f598917c76f7042421f2755d805fbb7864d5c
Madara <147428837+Madara1188@users.noreply.github.com>
2023-12-08 11:14:35 +0530

    Update autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001963474655
-                                and i.chat.id != -1002110737378
+                                and i.chat.id != -1002025766361
+                                and i.chat.id != -1001917426206
                             ):
                                 if left == 20:
                                     continue
