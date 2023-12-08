85606dd71811e70feaa93cb8992c68271aad1663
Madara <147428837+Madara1188@users.noreply.github.com>
2023-12-08 16:40:35 +0530

    Update queue.py

--- AnonXMusic/utils/inline/queue.py
+++ AnonXMusic/utils/inline/queue.py
@@ -72,8 +72,8 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="🍁ᴅᴇᴠᴇʟᴏᴘᴇʀ🍁", url="https://t.me/abhishekx9"),
-            InlineKeyboardButton(text="🍃sᴜᴘᴘᴏʀᴛ🍃", url="https://t.me/abhishek_bot_test"),
+            InlineKeyboardButton(text="🍁ᴅᴇᴠᴇʟᴏᴘᴇʀ🍁", url="https://t.me/"),
+            InlineKeyboardButton(text="🍃sᴜᴘᴘᴏʀᴛ🍃", url="https://t.me/"),
         ],
     ]
     return buttons
